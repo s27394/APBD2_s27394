e85bc7f2be16ad2598945e5b75eb13375b3e0740
s27394 <s27394@pjwstk.edu.pl>
2024-03-08 13:04:57 +0100

    add feature-calculating average

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -4,6 +4,7 @@ namespace ConsoleApplication1
 {
   internal class Program
   {
+    static int[] arr = { 1, 2, 3, 3, 4, 4, 4, 1, 2, 5, 6, 3, 1, 2, 3, 4, 1 };
     public static void Main(string[] args)
     {
       Console.WriteLine("HELLO FROM THE OTHER SIDEEE");
@@ -11,8 +12,21 @@ namespace ConsoleApplication1
       int x = 10;
       int y = 15;
       int z = 20;
+
+      double averageResult = calculateAverage(arr);
+      Console.WriteLine("calculated avg: " + averageResult);
       
       Console.WriteLine("GOOODINGHT!");
+    }
+
+    public static double calculateAverage(int[] arr)
+    {
+      int sum = 0;
+      foreach (var val in arr)
+      {
+        sum += val;
+      }
+      return (double)sum/(double)arr.Length;
     }
   }
 }
